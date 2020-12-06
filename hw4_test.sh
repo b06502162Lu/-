116cbbd995a6624001a385d6eaf855449ede10b0
Liang-Yu Lu <liang-yulu@Liang-Yus-MacBook-Pro.local>
2020-12-06 19:57:37 +0800

    update final

--- hw4_test.sh
+++ hw4_test.sh
@@ -4,15 +4,15 @@ gdown --id 1uVcw3TjcBguXDS7DnpzNqnUb8IiFr6rJ
 gdown --id 1kGPYna0WwWRvHGQoyHnX3m5_MOg3SNft
 
 #ckpt not yet
-gdown --id 1dB-AUiZXcVx5Fl9OGqIKef8wHLfeS55Z
-gdown --id 1_HDhYp7ZAO26jo4MgOo2s9dv9D7EKphw
-gdown --id 1dDh8D5ajP5j2gK1PD9amKsB7cY-SaoFR
-gdown --id 1vijEhUQAMdx4ih9FP12UcaG18oItIV3A
-gdown --id 1fo77rIr9ZqRJrbZshClqhsrOEE7fOQ3m
-gdown --id 1e9qO5TL_05qANM0_uEhgEFhkAqdFrBnE
-gdown --id 12ute23bCigcX3OAIow_17SZHEp04pD0L
-gdown --id 1MoCUzcMDFRjAv4cLG1bQ1iQkk0vzuB-P
-gdown --id 18EXg1PSdnpLVZHUdcoEOgvn3l8D0YT5w
-gdown --id 1f4deODTtLED9AZYPsysHukSUQvSygOK9
+gdown --id 1aEwrgHZKosCQjsktTYPuue-Bl5vx_KFK
+gdown --id 1-mJPTFWt9Yx11bQxVLn1BsYJpwRYDzyi
+gdown --id 19jF979yv-170wMMsMLx-TfkKX7--30Ln
+gdown --id 1N1KLuTu2jVuAtH3BMHoiiUlE0zhxVdRM
+gdown --id 1dIEzFVB3DkwU4zzIn1FJOWgfXWNqf69e
+gdown --id 1a6J-hBcoW4WlJnAxVY7JUGdd1qV02lkI
+gdown --id 1wuazjngSMRn8Dv3PgXrKgHGAH15NvmF1
+gdown --id 1TYtRYW9Q0n0iOXvo0VIWf3dDxWxT7-e2
+gdown --id 1JXqhEMsUKQzQ_79t5ZyzmTF4XDkfW1y5
+gdown --id 1AufHZXkKcqYBrwoxnuyr8uKoQazBBfhg
 
 python3 hw4_test.py $1 $2

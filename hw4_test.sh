85f485eefadc576c8103493135bac1ce846676ff
Liang-Yu Lu <liang-yulu@Liang-Yus-MacBook-Pro.local>
2020-12-08 09:01:53 +0800

    heyhey

--- hw4_test.sh
+++ hw4_test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-gdown --id 13gxXY5_Khh9Rx4qIZyZ8C13CYzVPoNnx
-gdown --id 1uVcw3TjcBguXDS7DnpzNqnUb8IiFr6rJ
-gdown --id 1kGPYna0WwWRvHGQoyHnX3m5_MOg3SNft
+gdown --id "13gxXY5_Khh9Rx4qIZyZ8C13CYzVPoNnx"
+gdown --id "1uVcw3TjcBguXDS7DnpzNqnUb8IiFr6rJ"
+gdown --id "1kGPYna0WwWRvHGQoyHnX3m5_MOg3SNft"
 
 #ckpt not yet
-gdown --id 1aEwrgHZKosCQjsktTYPuue-Bl5vx_KFK
-gdown --id 1-mJPTFWt9Yx11bQxVLn1BsYJpwRYDzyi
-gdown --id 19jF979yv-170wMMsMLx-TfkKX7--30Ln
-gdown --id 1N1KLuTu2jVuAtH3BMHoiiUlE0zhxVdRM
-gdown --id 1dIEzFVB3DkwU4zzIn1FJOWgfXWNqf69e
-gdown --id 1a6J-hBcoW4WlJnAxVY7JUGdd1qV02lkI
-gdown --id 1wuazjngSMRn8Dv3PgXrKgHGAH15NvmF1
-gdown --id 1TYtRYW9Q0n0iOXvo0VIWf3dDxWxT7-e2
-gdown --id 1JXqhEMsUKQzQ_79t5ZyzmTF4XDkfW1y5
-gdown --id 1AufHZXkKcqYBrwoxnuyr8uKoQazBBfhg
+gdown --id "1aEwrgHZKosCQjsktTYPuue-Bl5vx_KFK"
+gdown --id "1-mJPTFWt9Yx11bQxVLn1BsYJpwRYDzyi"
+gdown --id "19jF979yv-170wMMsMLx-TfkKX7--30Ln"
+gdown --id "1N1KLuTu2jVuAtH3BMHoiiUlE0zhxVdRM"
+gdown --id "1dIEzFVB3DkwU4zzIn1FJOWgfXWNqf69e"
+gdown --id "1a6J-hBcoW4WlJnAxVY7JUGdd1qV02lkI"
+gdown --id "1wuazjngSMRn8Dv3PgXrKgHGAH15NvmF1"
+gdown --id "1TYtRYW9Q0n0iOXvo0VIWf3dDxWxT7-e2"
+gdown --id "1JXqhEMsUKQzQ_79t5ZyzmTF4XDkfW1y5"
+gdown --id "1AufHZXkKcqYBrwoxnuyr8uKoQazBBfhg"
 
 python3 hw4_test.py $1 $2
